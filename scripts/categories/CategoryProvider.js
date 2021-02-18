cc4e1efe99252ce289fc8d114f5c267536d6fab4
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-18 13:36:22 -0600

    got menu items to render, cleared all initial errors

--- scripts/categories/CategoryProvider.js
+++ scripts/categories/CategoryProvider.js
@@ -2,14 +2,15 @@ import { bakeryAPI } from "../Settings.js"
 
 let categories = []
 
-export const useCategories = () => {
-  categories.slice()
-}
+export const useCategories = () => categories.slice()
+
 
 export const getCategories = () => {
   return fetch(`${bakeryAPI.baseURL}/categories`)
     .then(response => response.json())
     .then(categoriesArray => {
       categories = categoriesArray
+      console.log(categories)
     })
 }
+
--- scripts/products/ProductProvider.js
+++ scripts/products/ProductProvider.js
@@ -5,9 +5,9 @@ let products = []
 export const useProducts = () => products.slice()
 
 export const getProducts = () => {
-  fetch(`${bakeryAPI.baseURL}/products`)
-    .then(response => response.json())
-    .then(bakedGoods => {
-      products = bakedGoods
+  return fetch(`${bakeryAPI.baseURL}/products`)
+          .then(response => response.json())
+          .then(bakedGoods => {
+            products = bakedGoods
     })
 }
