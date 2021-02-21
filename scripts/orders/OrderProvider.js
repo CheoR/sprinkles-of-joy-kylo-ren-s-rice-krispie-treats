0cf4962d424049794045620be515e4175e6960cc
CheoR <cheo.roman+github@gmail.com>
2021-02-21 07:33:03 -0600

    fix: show alert and disable Place Order button if user is not logged in

--- scripts/orders/OrderProvider.js
+++ scripts/orders/OrderProvider.js
@@ -1,5 +1,6 @@
 import { bakeryAPI } from "../Settings.js"
 import { saveOrderProducts } from "./OrderProductProvider.js"
+import { authHelper } from "../auth/authHelper.js"
 
 const eventHub = document.querySelector("#container")
 
@@ -18,29 +19,38 @@ export const getOrders = () => {
 }
 
 export const saveOrder = (order, productsInOrder) => {
-  return fetch(`${bakeryAPI.baseURL}/orders`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(order)
-  })
-    .then(res => res.json())
-    .then(() => {
-      const orderProducts = productsInOrder.map(product => {
-        return {
-          "orderId": order.id,
-          "productId": product.id
-        }
-      })
-      console.info("OrderProvider.js - saveOrder")
-      console.table(orderProducts)
-      productsInOrder = [] //change
-      return saveOrderProducts(orderProducts)
+
+  if(authHelper.isUserLoggedIn()) {
+    /*
+      Either restrict user ability to place order if not logged in, or add disabled class to button if 
+      user is not logged in.
+    */
+    return fetch(`${bakeryAPI.baseURL}/orders`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(order)
     })
-    .then(() => getOrders())
-    .then(dispatchStateChangeEvent)
-}
+      .then(res => res.json())
+      .then(() => {
+        const orderProducts = productsInOrder.map(product => {
+          return {
+            "orderId": order.id,
+            "productId": product.id
+          }
+        })
+        console.info("OrderProvider.js - saveOrder")
+        console.table(orderProducts)
+        productsInOrder = [] //change
+        return saveOrderProducts(orderProducts)
+      })
+      .then(() => getOrders())
+      .then(dispatchStateChangeEvent)
+  } else {
+      alert("YOu must be logged in to place order")
+  }
+} // saveOrder
 
 const dispatchStateChangeEvent = () => {
   const ordersStateChangedEvent = new CustomEvent("ordersStateChanged")
